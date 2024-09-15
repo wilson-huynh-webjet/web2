8fcba8165396952f212b7ab08ce477b15459412b
wilson-huynh-webjet <wilson.huynh@webjet.com.au>
2024-09-15 14:18:13 +1000

    add submodule

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ShareApp from '../shareweb/src/App'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <ShareApp />
       </p>
     </>
   )
