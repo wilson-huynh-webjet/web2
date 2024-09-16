e9125e54875167946e6c7614b7083f8be12fde9e
wilson-huynh-webjet <wilson.huynh@webjet.com.au>
2024-09-16 18:12:45 +1000

    Update button UI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import ShareApp from '../shareweb/src/App'
+import Button from '@webjet/react/components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Web 2</h1>
+      <h1>Main Web App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </Button>        
       </div>
       <p className="read-the-docs">
         <ShareApp />
